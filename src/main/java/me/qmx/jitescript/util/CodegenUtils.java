62fef6ba2e04a3fa61ec1384ccf8f96bbd5548a9
Douglas Campos <qmx@qmx.me>
2011-05-19 18:07:17 -0300

    borrowing code from jruby \o/

---
+++ src/main/java/me/qmx/jitescript/util/CodegenUtils.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.qmx.jitescript.util;
+
+/**
+ *
+ * @author qmx
+ */
+public class CodegenUtils {
+    
+}
