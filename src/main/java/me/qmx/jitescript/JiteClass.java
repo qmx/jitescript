85516d7c0402bc273f1ea999ca97e28511086f6a
Douglas Campos <qmx@qmx.me>
2011-05-21 02:26:36 -0300

    implementation start

--- src/main/java/me/qmx/jitescript/JiteClass.java
+++ src/main/java/me/qmx/jitescript/JiteClass.java
@@ -1,5 +1,6 @@
 package me.qmx.jitescript;
 
+import org.objectweb.asm.MethodVisitor;
 import java.util.ArrayList;
 import java.util.List;
 import org.objectweb.asm.ClassWriter;
@@ -13,7 +14,6 @@ import static me.qmx.jitescript.util.CodegenUtils.*;
 public class JiteClass implements Opcodes {
 
     private final List<MethodDefinition> methods = new ArrayList<MethodDefinition>();
-    private ClassWriter cw;
     private final String className;
 
     public JiteClass(String className) {
@@ -25,10 +25,18 @@ public class JiteClass implements Opcodes {
     }
 
     byte[] toBytes() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        ClassWriter cw = new ClassWriter(0);
+        cw.visit(V1_7, ACC_PUBLIC + ACC_SUPER, this.className, null, "java/lang/Object", null);
+        for(MethodDefinition def : methods){
+            MethodVisitor mv = cw.visitMethod(def.getModifiers(), def.getMethodName(), def.getSignature(), null, null);
+            mv.visitCode();
+            MethodBody methodBody = def.getMethodBody();
+            methodBody.setMethodVisitor(mv);
+            methodBody.executableMethodBody(mv);
+            mv.visitEnd();
+        }
+        cw.visitEnd();
+        return cw.toByteArray();
     }
 
-    private void init() {
-        cw = new ClassWriter(0);
-    }
 }
