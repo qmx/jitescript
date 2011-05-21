84a893620bb480160471b3a71b402c07146fd4b9
Douglas Campos <qmx@qmx.me>
2011-05-21 10:10:30 -0300

    extracting class name

--- src/test/java/me/qmx/jitescript/JiteClassTest.java
+++ src/test/java/me/qmx/jitescript/JiteClassTest.java
@@ -1,6 +1,8 @@
 package me.qmx.jitescript;
 
 import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import static me.qmx.jitescript.util.CodegenUtils.*;
 import org.junit.Test;
 import org.objectweb.asm.MethodVisitor;
@@ -11,9 +13,6 @@ import org.objectweb.asm.MethodVisitor;
  */
 public class JiteClassTest {
 
-    @Test
-    public void testDSL() {
-        JiteClass jiteClass = new JiteClass("invokedynamic") {
     public static class DynamicClassLoader extends ClassLoader {
 
         public Class<?> define(String className, byte[] bytecode) {
@@ -22,6 +21,10 @@ public class JiteClassTest {
     };
 
 
+    @Test
+    public void testDSL() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        final String className = "helloTest";
+        JiteClass jiteClass = new JiteClass(className) {
             {
                 defineMethod("main", ACC_PUBLIC + ACC_STATIC, sig(void.class, String[].class), new MethodBody() {
 
