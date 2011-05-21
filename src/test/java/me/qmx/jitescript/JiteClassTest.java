ec54c876743773e00c0d0320df0fc7753d4fbb7f
Douglas Campos <qmx@qmx.me>
2011-05-21 10:09:29 -0300

    adding test classloader

--- src/test/java/me/qmx/jitescript/JiteClassTest.java
+++ src/test/java/me/qmx/jitescript/JiteClassTest.java
@@ -14,6 +14,13 @@ public class JiteClassTest {
     @Test
     public void testDSL() {
         JiteClass jiteClass = new JiteClass("invokedynamic") {
+    public static class DynamicClassLoader extends ClassLoader {
+
+        public Class<?> define(String className, byte[] bytecode) {
+            return super.defineClass(className, bytecode, 0, bytecode.length);
+        }
+    };
+
 
             {
                 defineMethod("main", ACC_PUBLIC + ACC_STATIC, sig(void.class, String[].class), new MethodBody() {
