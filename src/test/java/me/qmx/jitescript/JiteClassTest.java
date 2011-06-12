fc9c23e3b5b94950aae818ffd90b897833bc19fa
Douglas Campos <qmx@qmx.me>
2011-06-11 23:30:24 -0300

    formatting

--- src/test/java/me/qmx/jitescript/JiteClassTest.java
+++ src/test/java/me/qmx/jitescript/JiteClassTest.java
@@ -36,29 +36,22 @@ public class JiteClassTest {
         }
     }
 
-    ;
-
     @Test
     public void testDSL() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         final String className = "helloTest";
-        JiteClass jiteClass = new JiteClass(className) {
-
-            {
-                defineMethod("main", ACC_PUBLIC | ACC_STATIC, sig(void.class, String[].class), new MethodBody() {
-                    {
-                        ldc("helloWorld");
-                        getstatic(p(System.class), "out", ci(PrintStream.class));
-                        swap();
-                        invokevirtual(p(PrintStream.class), "println", sig(void.class, Object.class));
-                        voidreturn();
-                    }
-                });
-                defineMethod("hello", ACC_PUBLIC | ACC_STATIC, sig(String.class), new MethodBody() {{
-                    ldc("helloWorld");
-                    areturn();
-                }});
-            }
-        };
+        JiteClass jiteClass = new JiteClass(className) {{
+            defineMethod("main", ACC_PUBLIC | ACC_STATIC, sig(void.class, String[].class), new MethodBody() {{
+                ldc("helloWorld");
+                getstatic(p(System.class), "out", ci(PrintStream.class));
+                swap();
+                invokevirtual(p(PrintStream.class), "println", sig(void.class, Object.class));
+                voidreturn();
+            }});
+            defineMethod("hello", ACC_PUBLIC | ACC_STATIC, sig(String.class), new MethodBody() {{
+                ldc("helloWorld");
+                areturn();
+            }});
+        }};
 
         byte[] classBytes = jiteClass.toBytes();
 
